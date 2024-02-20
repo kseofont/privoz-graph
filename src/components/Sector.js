0a0127177a2868e7281595650962530ea2362d75
Yaroslav Andrieiev <kseofon@gmail.com>
2024-02-21 00:56:44 +0200

    Styles

--- src/components/Sector.js
+++ src/components/Sector.js
@@ -12,11 +12,11 @@ function Sector({ sector, players, handleAddTrader }) {
             <div className={`sector border p-3 mb-3 h-100 ${sector.name.toLowerCase()}`}>
                 <h4>{sector.name}</h4>
                 <div className="">
-                    <div className={`row `}>
+                    <div className={`justify-content-between d-flex`}>
 
                         {sector.traders.length > 0 && sector.traders.map(trader => (
                             trader.player && (
-                                <div key={trader.id} className={`d-flex flex-column ustify-content-center align-items-center gap-1 ${columnClass}`} >
+                                <div key={trader.id} className={`trader d-flex flex-column ustify-content-center align-items-center gap-1 ${columnClass}`} >
 
                                     <i className='bi bi-person d-flex justify-content-center align-items-center' style={{ color: trader.player.hero }}></i>
                                     <p>{trader.player.id}</p>
