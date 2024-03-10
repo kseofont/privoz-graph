c7dac254d5727141f429bb8c8e7e29e4114210b2
Yaroslav Andrieiev <kseofon@gmail.com>
2024-03-10 16:12:05 +0200

    Server fix

--- src/components/Login.js
+++ src/components/Login.js
@@ -36,9 +36,7 @@ function Login() {
                         ))}
                     </Dropdown.Menu>
                 </Dropdown>
-                <div className="test"> <h4>show me WITH GOD BLESS</h4></div>
-                <div className="test"> <h4>v991</h4></div>
-                <div className="test"> <h4>success</h4></div>
+
             </div>
         </div>
     );
