f0bfb9fe5c2105b0c2b193ce1ae907b5ea072131
Yaroslav Andrieiev <kseofon@gmail.com>
2024-03-10 16:33:11 +0200

    Styles fix

--- src/components/CurrentPlayer.js
+++ src/components/CurrentPlayer.js
@@ -8,41 +8,47 @@ function CurrentPlayer({ player }) {
 
     return (
         <div key={player.id} className={'p-2'} style={{ color: player.hero.color }}>
-            <div className={'player ' + player.hero.name.replace(/\s+/g, '-').toLowerCase()}>
-                {traderCount > 0 && (
-                    <div className='trader-count'>
-                        <p>{traderCount} trader{traderCount !== 1 ? 's' : ''} in sector {player.sector}</p>
+            <div className="d-flex flex-column">
 
 
+                <div className={'player ' + player.hero.name.replace(/\s+/g, '-').toLowerCase()}>
+                    {traderCount > 0 && (
+                        <div className='trader-count'>
+                            <p>{traderCount} trader{traderCount !== 1 ? 's' : ''} in sector {player.sector}</p>
+
+
+                        </div>
+
+                    )}
+                </div>
+                <div className="d-flex flex-column align-items-center justify-content-center" style={{ maxWidth: '600px', margin: '0 auto' }}>
+                    <h3>{player.hero.name}</h3>
+                    <div className="player-info d-flex justify-content-between w-100">
+
+                        <div className='d-flex align-items-center flex-column '>
+                            <img src={`${process.env.PUBLIC_URL}/img/coin.webp`} alt="Coins" className='coins' /> {player.coins}
+                        </div>
+                        <div className='d-flex align-items-center flex-column '>
+                            <img src={`${process.env.PUBLIC_URL}/img/trader-img.webp`} alt="Trader" className='trader-img' />
+                            {Number.isInteger(player.traders?.length) ? player.traders.length : 0}
+                        </div>
+                        <div className='d-flex align-items-center flex-column '>
+                            <img src={`${process.env.PUBLIC_URL}/img/event-card-img.webp`} alt="Product Card" className='event-card-img product-card-img' />
+                            {Number.isInteger(player.productCards?.length) ? player.productCards.length : 0}
+                        </div>
+
+                        {player.hero.image && (
+                            <div className='d-flex align-items-center flex-column'>
+                                <img src={player.hero.image} alt={player.hero.name} style={{ width: '50px', height: '50px' }} />
+                            </div>
+                        )}
                     </div>
+                </div>
 
-                )}
             </div>
 
             <div className="d-flex flex-column ">
-                <h3>{player.hero.name}</h3>
-                <div className="player-info d-flex justify-content-between">
 
-                    <div className='d-flex align-self-center align-items-center flex-column '>
-                        <img src={`${process.env.PUBLIC_URL}/img/coin.webp`} alt="Coins" className='coins' /> {player.coins}
-                    </div>
-                    <div className='d-flex align-self-center align-items-center flex-column '>
-                        <img src={`${process.env.PUBLIC_URL}/img/trader-img.webp`} alt="Trader" className='trader-img' />
-                        {Number.isInteger(player.traders?.length) ? player.traders.length : 0}
-                    </div>
-                    <div className='d-flex align-self-center align-items-center flex-column '>
-                        <img src={`${process.env.PUBLIC_URL}/img/event-card-img.webp`} alt="Product Card" className='event-card-img product-card-img' />
-                        {Number.isInteger(player.productCards?.length) ? player.productCards.length : 0}
-
-                    </div>
-
-
-
-
-
-                    {/* If you want to display the hero's image, ensure you have a valid src. If it's empty, you might want to handle it accordingly. */}
-                    {player.hero.image && <div>Hero Image: <img src={player.hero.image} alt={player.hero.name} style={{ width: '50px', height: '50px' }} /></div>}
-                </div>
                 <div className="cards-info d-flex justify-content-between">
                     {player.productCards.map(productCard => (
                         console.log('productCard', productCard),
--- src/components/Login.js
+++ src/components/Login.js
@@ -19,27 +19,28 @@ function Login() {
 
         fetchData();
     }, []);
-
     return (
-        <div className="login">
-            <div className="login-title">
-                <h3>Login</h3>
-            </div>
-            <div className="login-players">
-                <Dropdown>
-                    <Dropdown.Toggle variant="primary" id="login-players__dropdown">
-                        Select Players
-                    </Dropdown.Toggle>
-                    <Dropdown.Menu>
-                        {players.map((player) => (
-                            <Dropdown.Item href={"/game?player=" + player.id}> Player: {player.id}</Dropdown.Item>
-                        ))}
-                    </Dropdown.Menu>
-                </Dropdown>
-
+        <div className="login vh-100 d-flex align-items-center justify-content-center"> {/* Added classes here */}
+            <div className="text-center"> {/* Optionally add "text-center" for text alignment */}
+                <div className="login-title mb-4"> {/* Added margin-bottom for spacing */}
+                    <h3>Login</h3>
+                </div>
+                <div className="login-players">
+                    <Dropdown>
+                        <Dropdown.Toggle variant="primary" id="login-players__dropdown">
+                            Select Players
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            {players.map((player, index) => (
+                                <Dropdown.Item key={index} href={"/game?player=" + player.id}> Player: {player.id}</Dropdown.Item>
+                            ))}
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </div>
             </div>
         </div>
     );
+
 }
 
 export default Login;
